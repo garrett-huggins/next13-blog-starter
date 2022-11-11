fcc2b9ac68dabc29b9d2e7168613c05d842ccea4
Crow <garretthugs@gmail.com>
2022-11-11 18:25:47 -0500

    Create navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="bg-neutral-800">
+      <nav className="container py-4 mx-auto">
+        <ul className="flex space-x-6 text-lg justify-center">
+          <li>
+            <NavLink href="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink href="/blog">Blog</NavLink>
+          </li>
+          <li>
+            <NavLink href="https://github.com/garrett-huggins/next13-blog-starter">
+              Github
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+type NavLink = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const NavLink = ({ href, children }: NavLink) => {
+  return (
+    <Link className="hover:text-gray-300 hover:underline" href={href}>
+      {children}
+    </Link>
+  );
+};
