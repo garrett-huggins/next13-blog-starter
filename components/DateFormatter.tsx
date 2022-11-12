cc7014d09c988608f7cfe4bfb9ba8d9ff4979a90
Crow <garretthugs@gmail.com>
2022-11-11 21:19:50 -0500

    create date formatter using date-fns

---
+++ components/DateFormatter.tsx
@@ -0,0 +1,16 @@
+import { parseISO, format } from "date-fns";
+
+type Props = {
+  dateString: string;
+};
+
+const DateFormatter = ({ dateString }: Props) => {
+  const date = parseISO(dateString);
+  return (
+    <time className="text-slate-400" dateTime={dateString}>
+      {format(date, "LLLL	d, yyyy")}
+    </time>
+  );
+};
+
+export default DateFormatter;
