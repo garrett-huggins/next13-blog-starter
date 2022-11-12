9d97a22fbf463ddbc41730be95d4f066776af71c
Crow <garretthugs@gmail.com>
2022-11-11 22:10:33 -0500

    update post preview

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -9,12 +9,13 @@ type Items = {
 export default function PostPreview({ post }: { post: Items }) {
   return (
     <Link href={`/posts/${post.slug}`}>
-      <div className="max-w-[400px] space-y-2 mx-auto group">
+      <div className="md:max-w-md space-y-2 mx-auto group">
         <Image
           alt={`cover image for ${post.title}`}
           src={post.coverImage}
           width={400}
           height={400}
+          style={{ width: "100%" }}
         />
         <p className="font-semibold text-xl group-hover:underline">
           {post.title}
