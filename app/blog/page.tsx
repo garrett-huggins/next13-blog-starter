7cd9e73f6221cada989793859cd12d2f997f1df8
Crow <garretthugs@gmail.com>
2022-11-11 22:54:07 -0500

    update blog home page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,21 @@
-export default function About() {
-  return <div></div>;
+import { getAllPosts } from "../../lib/api";
+import PostPreview from "../../components/PostPreview";
+
+export default function Blog() {
+  const posts = getAllPosts(["title", "date", "excerpt", "coverImage", "slug"]);
+
+  return (
+    <div className="container mx-auto px-5">
+      <main>
+        <h1 className="text-center text-3xl mb-10">All Posts</h1>
+        <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-32 gap-8">
+          {posts.map((post) => (
+            <div>
+              <PostPreview post={post} />
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
