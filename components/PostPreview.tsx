e3edd3d613b615a9e73d7064e716b5dd4402822b
Crow <garretthugs@gmail.com>
2022-11-11 21:19:31 -0500

    create post preview card

---
+++ components/PostPreview.tsx
@@ -0,0 +1,27 @@
+import DateFormatter from "./DateFormatter";
+import Image from "next/image";
+import Link from "next/link";
+
+type Items = {
+  [key: string]: string;
+};
+
+export default function PostPreview({ post }: { post: Items }) {
+  return (
+    <Link href={`/posts/${post.slug}`}>
+      <div className="max-w-[400px] space-y-2 mx-auto group">
+        <Image
+          alt={`cover image for ${post.title}`}
+          src={post.coverImage}
+          width={400}
+          height={400}
+        />
+        <p className="font-semibold text-xl group-hover:underline">
+          {post.title}
+        </p>
+        <DateFormatter dateString={post.date} />
+        <div>{post.excerpt}</div>
+      </div>
+    </Link>
+  );
+}
